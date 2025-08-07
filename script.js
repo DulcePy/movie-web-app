9181c1f3d58febeecdaf7e1a761e17bc2555accc
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2025-08-07 17:52:42 -0300

    Refactor movie display logic and update styles for improved layout and responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-import config from './config.js';
+import config from "./config.js";
 const apiKey = config.API_KEY;
+const apiURLAside = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&page=1`;
 const apiURL = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&page=3`;
 
 async function fetchTrendingMovies() {
@@ -12,11 +13,21 @@ async function fetchTrendingMovies() {
   }
 }
 
+async function fetchTrendingMoviesAside() {
+  try {
+    const response = await fetch(apiURLAside);
+    const data = await response.json();
+    displayMoviesAside(data.results);
+  } catch (error) {
+    console.error("Error fetching API data: ", error);
+  }
+}
+
 function displayMovies(movies) {
   const container = document.getElementById("movie-container");
   container.innerHTML = "";
 
-  const limitedMovies = movies.slice(0, 20); // limit to 6 movies
+  const limitedMovies = movies.slice(0, 20);
 
   limitedMovies.forEach((movie) => {
     const year = movie.release_date ? movie.release_date.split("-")[0] : "N/A";
@@ -44,7 +55,7 @@ function displayMovies(movies) {
                 1
               )}
             </h3>
-            <h3>Language: ${language}</h3>
+            <h3>Language: <span class="uppercase">${language}</span></h3>
             <h3>Release: ${year}</h3>
           </div>
         </div>
@@ -55,4 +66,63 @@ function displayMovies(movies) {
   });
 }
 
+function displayMoviesAside(movies) {
+  const bannerPoster = document.getElementById("banner-poster");
+  const posterElement = document.getElementById("poster");
+  const posterTitle = document.getElementById("poster-title");
+  const posterDescription = document.getElementById("poster-description");
+  const posterLanguaje = document.getElementById("poster-lang");
+  const posterRating = document.getElementById("poster-rating");
+
+  const asideContainer = document.getElementById("aside-card");
+  asideContainer.innerHTML = "";
+  const shuffledMovies = movies.sort(() => Math.random() - 0.5);
+  const randomMovies = shuffledMovies.slice(0, 3);
+
+  randomMovies.forEach((movie) => {
+    const language = movie.original_language ? movie.original_language : "N/A";
+    bannerPoster.src = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
+    bannerPoster.alt = movie.title;
+    posterElement.src = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
+    posterElement.alt = movie.title;
+    posterTitle.innerHTML = movie.title;
+    posterDescription.innerHTML = movie.overview;
+    posterLanguaje.innerHTML = `Language: <span class="uppercase">${language}</span>`;
+    posterRating.innerHTML = `Rating: ${
+      movie.vote_average ? movie.vote_average.toFixed(1) : "N/A"
+    }`;
+
+    const movieCard = document.createElement("div");
+    movieCard.className = "flex items-center gap-4 mb-4";
+
+    movieCard.innerHTML = `
+      <img class="w-24 h-[100%]" 
+        src="https://image.tmdb.org/t/p/w200${movie.poster_path}" 
+        alt="${movie.title}">
+
+      <div class="text-zinc-300">
+      <h2 class="text-gray-100 text-left md:text-[16px] text-sm font-semibold line-clamp-1 w-fit">
+            ${movie.title}
+          </h2>
+        <p>${
+          movie.overview
+            ? movie.overview.slice(0, 100) + "..."
+            : "No description available"
+        }</p>
+        <p>Language: <span class="uppercase">${language}</span></p>
+        <p>
+          <span class="text-yellow-400">
+            <i class="fa-solid fa-star"></i>
+          </span> 
+          Rating: ${movie.vote_average ? movie.vote_average.toFixed(1) : "N/A"}
+        </p>
+      </div>
+    `;
+
+    asideContainer.appendChild(movieCard);
+  });
+}
+
+// Iniciar las peticiones
 fetchTrendingMovies();
+fetchTrendingMoviesAside();
